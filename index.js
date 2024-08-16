4599708d6936f6e265fc1630c1e213a79e90f18b
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2024-08-16 18:02:25 -0500

    Version inicial

---
+++ index.js
@@ -0,0 +1,67 @@
+require("dotenv").config();
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var indexRouter = require("./routes/index");
+
+var app = express();
+
+const rateLimit = require("express-rate-limit");
+const cors = require("cors");
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minute
+    limit: 180, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+    // store: ... , // Redis, Memcached, etc. See below.
+});
+
+//  apply to all requests
+if (process.env.NODE_ENV === "production") {
+    app.use(
+        cors({
+            origin: ["https://registro-consulta-previa.onrender.com"],
+        })
+    );
+} else {
+    app.use(
+        cors({
+            origin: ["http://localhost:7153"],
+        })
+    );
+}
+
+app.use(limiter);
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/v1", indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
+});
+
+module.exports = app;
