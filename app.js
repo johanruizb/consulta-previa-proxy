5c4c06989708ce0f577feef4e1d29a9d9c627119
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2024-08-16 18:40:03 -0500

    chore: Update CORS configuration for app.js and vercel.json

---
+++ app.js
@@ -0,0 +1,58 @@
+require("dotenv").config();
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var indexRouter = require("./routes/index");
+
+var app = express();
+
+const rateLimit = require("express-rate-limit");
+const cors = require("cors");
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minute
+    limit: 180, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+    // store: ... , // Redis, Memcached, etc. See below.
+});
+
+const origin =
+    app.get("env") == "development"
+        ? "http://localhost:7153"
+        : "https://registro-consulta-previa.onrender.com";
+
+app.use(cors({ origin: origin, credentials: true }));
+app.use(limiter);
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/v1", indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
+});
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,72 @@
+require("dotenv").config();
+
+var express = require("express");
+var router = express.Router();
+const createError = require("http-errors");
+
+/* GET home page. */
+router.get("/countries", async function (req, res, next) {
+    try {
+        const result = await fetch(
+            "https://api.countrystatecity.in/v1/countries",
+            {
+                headers: {
+                    "X-CSCAPI-KEY": process.env.X_CSCAPI_KEY,
+                },
+            },
+        );
+
+        res.header("Cache-Control", "public, max-age=604800");
+
+        const data = await result.json();
+        res.json(data);
+    } catch (error) {
+        console.error(error);
+        next(createError(500));
+    }
+});
+
+router.get("/countries/:isoCode/states", async function (req, res, next) {
+    try {
+        const result = await fetch(
+            `https://api.countrystatecity.in/v1/countries/${req.params.isoCode}/states`,
+            {
+                headers: {
+                    "X-CSCAPI-KEY": process.env.X_CSCAPI_KEY,
+                },
+            },
+        );
+
+        res.header("Cache-Control", "public, max-age=604800");
+
+        const data = await result.json();
+        res.json(data);
+    } catch (error) {
+        console.error(error);
+        next(createError(500));
+    }
+});
+
+router.get(
+    "/countries/:isoCode/states/:stateCode/cities",
+    async function (req, res, next) {
+        try {
+            const result = await fetch(
+                `https://api.countrystatecity.in/v1/countries/${req.params.isoCode}/states/${req.params.stateCode}/cities`,
+                {
+                    headers: {
+                        "X-CSCAPI-KEY": process.env.X_CSCAPI_KEY,
+                    },
+                },
+            );
+            res.header("Cache-Control", "public, max-age=604800");
+            const data = await result.json();
+            res.json(data);
+        } catch (error) {
+            console.error(error);
+            next(createError(500));
+        }
+    },
+);
+
+module.exports = router;
