e158a6b9b9a4740f076ebab8c14167a6372dce31
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2024-08-17 14:27:32 -0500

    chore: Update npm dependency to latest stable version

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -16,7 +16,7 @@ module.exports = {
             repo: "git@github.com:johanruizb/consulta-previa-proxy.git",
             path: "/home/ubuntu/registro-consulta-previa",
             "post-deploy":
-                "npm install && pm2 reload ecosystem.config.js --env production",
+                'export PATH="/home/ubuntu/.nvm/versions/node/v20.16.0/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin && npm install && pm2 reload ecosystem.config.js --env production',
         },
     },
 };
