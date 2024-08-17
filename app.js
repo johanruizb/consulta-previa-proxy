92eddda885a207e8b20043b567eaebff3df4ef6b
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2024-08-17 08:27:47 -0500

    chore: Update file path for serving index.html file

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.use(function (err, req, res, next) {
 
 // Handles any requests that don't match the ones above
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+    res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
 module.exports = app;
